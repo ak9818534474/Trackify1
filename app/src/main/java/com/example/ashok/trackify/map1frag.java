eb361ddf9a1631f4d11c1539d49a400f2601da7c
Aditya Chawla <aditya21196@gmail.com>
2016-07-20 21:28:57 +0530

    added map fragment

---
+++ app/src/main/java/com/example/ashok/trackify/map1frag.java
@@ -0,0 +1,28 @@
+package com.example.ashok.trackify;
+
+/**
+ * Created by Acer on 7/20/2016.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class map1frag extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_maps, container, false);
+
+
+
+
+
+        return view;
+    }
+
+}
